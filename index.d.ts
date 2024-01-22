eb1ebe7545e170e7683f63a580c32d1750f2dfbd
afeiship <1290657123@qq.com>
2024-01-22 20:42:22 +0800

    feat: add computed state

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
+interface IValtioOptions {
+  getters?: Record<string, (store: any) => any>;
+}
+
 interface NxStatic {
   ReactValtio: any;
-  $valtio: (initialState: T) => { store: T; state: T };
+  $valtio: (initialState: T, options: IValtioOptions) => { store: T; state: any };
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import nx from '@jswork/next';
 import { proxy, useSnapshot } from 'valtio';
+import { derive } from 'derive-valtio';
 import { useRef } from 'react';
+
+const defaults = { getters: {} };
 
 const NxReactValtio = nx.declare('nx.ReactValtio', {
   statics: {
     init: function () {
-      nx.$valtio = (initialState) => {
+      nx.$valtio = (initialState, inOptions) => {
         const store = useRef(proxy(initialState)).current;
-        const state = useSnapshot(store);
-        return { store, state };
+        const snapshot = useSnapshot(store);
+        const options = nx.mix(null, defaults, inOptions);
+        const gettersEntries = {};
+
+        nx.forIn(options.getters, (key, value) => {
+          const getterFn = () => value(store);
+          gettersEntries[key] = getterFn;
+        });
+
+        const derived = derive(gettersEntries);
+        const state = { ...snapshot, ...derived };
+        return { state, store };
       };
     }
   }
