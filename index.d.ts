f8e27585ea7ecc03bbcc9f508786c65364a3c952
aric.zheng <1290657123@qq.com>
2025-06-21 23:43:14 +0800

    fix: typing for do.valtio

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ type IGetters = Record<string, (store: any) => any>;
 
 interface NxStatic {
   ReactValtio: any;
-  $valtio: (initialState: T, getters?: IGetters) => { store: T; state: any; [key: string]: any };
+  $valtio: <T>(initialState: T, getters?: IGetters) => { store: T; state: any; [key: string]: any };
 }
